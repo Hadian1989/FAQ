a4add3fa4e910694ab5ad56d8876bea725f102da
ghazal1989 <ghazal.hadiyan@gmail.com>
2021-09-09 12:31:38 +0430

    create faq app and initial settings

---
+++ faq/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include
+from .models import *
+
+admin.site.site_header = 'FAQ Dashboard'
+
+
+# Register your models here.
+urlpatterns = [
+path('admin/', admin.site.urls),
+path('', include('pages.urls')), # new
+]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # readonly_fields = ['',]
+    list_display = ('name',)
+    list_filter = ['name', ]
+    list_per_page = 20
+    actions_selection_counter = True
+    date_hierarchy = 'name'
+    search_fields = ('name',)
+    ordering = ('name', )
